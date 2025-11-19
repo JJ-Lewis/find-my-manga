85918e9df17f60eefb55e7f49ccdaba18a31759a
jlewisgenex <James.Lewis@genexinstitute.com>
2025-11-19 13:43:11 +0200

    continue scaffolding

---
+++ app/Services/Availability/AggregatorService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\Availability;
+
+class AggregatorService
+{
+
+}
---
+++ app/Services/Retailer/Adapters/TestRetailerAdapter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\Retailer\Adapters;
+
+class TestRetailerAdapter
+{
+
+}
